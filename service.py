e0beca2b834603e9929b7225dd97958c4ba63b19
Ramiro Morales <ramiro@rmorales.net>
2015-12-03 21:29:37 -0300

    Version 0.2.2. Set it independently in service.py.
    This means now we need to remember to update it in that archive too
    before cutting a release.
    
    But it also allows us to know exactly which version of the addon users
    are running when they submit logs if they've manually downloaded
    service.py from the surce code repository.

--- service.py
+++ service.py
@@ -42,7 +42,7 @@ __addon__ = xbmcaddon.Addon()
 __author__     = __addon__.getAddonInfo('author')
 __scriptid__   = __addon__.getAddonInfo('id')
 __scriptname__ = __addon__.getAddonInfo('name')
-__version__    = __addon__.getAddonInfo('version')
+__version__    = '0.2.2'
 __language__   = __addon__.getLocalizedString
 
 __cwd__        = xbmc.translatePath(__addon__.getAddonInfo('path')).decode("utf-8")
