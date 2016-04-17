ebbc5bd447fb409994cd5ec7d93827786a8b05a8
Ramiro Morales <ramiro@rmorales.net>
2016-04-17 19:58:22 -0300

    Micro version 0.2.3.

--- service.py
+++ service.py
@@ -41,7 +41,7 @@ __addon__ = xbmcaddon.Addon()
 __author__     = __addon__.getAddonInfo('author')
 __scriptid__   = __addon__.getAddonInfo('id')
 __scriptname__ = __addon__.getAddonInfo('name')
-__version__    = '0.2.2'
+__version__    = '0.2.3'
 __language__   = __addon__.getLocalizedString
 
 __cwd__        = xbmc.translatePath(__addon__.getAddonInfo('path')).decode("utf-8")
