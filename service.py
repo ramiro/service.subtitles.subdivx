cb0edcf580af95bd5f119d34bbf100fc4c143587
Ramiro Morales <cramm0@gmail.com>
2017-01-23 19:05:03 -0300

    Start 0.3.0 deveploment cycle.
    The following notes apply:
    
    a) We reserve versions 0.2.6 - 0.2.9 for any eventual bug fix release of
       0.2.5 we should need to cut in case we receive reports of errors or
       regressions. We will create a `0.2.5-maintenance` branch to perform
       this work there if needed.
    
       In retrospect, at least one of the 0.2.1 - 0.2.5 releases should have
       been named 0.3.0, etc. but alas. I didn'think about semver back then.
    
    b) We will try to update the version (0.2.11, 0.2.12, ...) every time we
       implement a new feature in our path to 0.3.0. This way, if users
       install work-in-progress ZIP files from GitHub they can report us
       such version IDs and these versions still will increment monotonica-
       lly. I don't think we need to release these versions to the Kodi
       addons repo.
    
       We will set the version to 0.3.0 just before releasing it to Kodi
       addon repos.

--- service.py
+++ service.py
@@ -44,7 +44,7 @@ __addon__ = xbmcaddon.Addon()
 __author__     = __addon__.getAddonInfo('author')
 __scriptid__   = __addon__.getAddonInfo('id')
 __scriptname__ = __addon__.getAddonInfo('name')
-__version__    = '0.2.5'
+__version__    = '0.2.10'
 __language__   = __addon__.getLocalizedString
 
 __cwd__        = xbmc.translatePath(__addon__.getAddonInfo('path')).decode("utf-8")
