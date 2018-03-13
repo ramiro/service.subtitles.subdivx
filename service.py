1c2146bb084671bab90a8e2c7021d2550dde5922
Ramiro Morales <cramm0@gmail.com>
2018-03-13 20:27:29 -0300

    Release 0.3.0 with all changes since 0.2.5.

--- service.py
+++ service.py
@@ -45,7 +45,7 @@ __addon__ = xbmcaddon.Addon()
 __author__     = __addon__.getAddonInfo('author')
 __scriptid__   = __addon__.getAddonInfo('id')
 __scriptname__ = __addon__.getAddonInfo('name')
-__version__    = '0.2.14'
+__version__    = '0.3.0'
 __language__   = __addon__.getLocalizedString
 
 __cwd__        = xbmc.translatePath(__addon__.getAddonInfo('path')).decode("utf-8")
